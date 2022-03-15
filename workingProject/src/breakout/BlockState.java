c062c64b3283a6134c420bc7a50facae933043a6
Yuke Cheng <yukecheng0214@hotmail.com>
2022-03-16 00:38:36 +0100

    Add access modifiers to BlockState

--- workingProject/src/breakout/BlockState.java
+++ workingProject/src/breakout/BlockState.java
@@ -3,15 +3,15 @@ package breakout;
 public class BlockState {
 	// TODO: implement
 	//DONE-check it
-	Point blockTL1;
-	Point blockBR1;
-	Vector size1;
-	BlockState (Point blockTL, Point blockBR,Vector size){
+	private Point blockTL1;
+	private Point blockBR1;
+	private Vector size1;
+	public BlockState (Point blockTL, Point blockBR,Vector size){
 		this.blockBR1=blockBR;
 		this.blockTL1=blockTL;
 		this.size1=size;
 	};
-	public Point  getPosition() {
+	public Point getPosition() {
 		int blockcenterx= (blockBR1.getX()-blockTL1.getX())/2;
 		int blockcentery= (blockTL1.getY()-blockBR1.getY())/2;
 		Point center=new Point (blockcenterx,blockcentery);
