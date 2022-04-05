05cebe8aa2b1669f021361bf7aa16c18ccf7e802
Yuke Cheng <yukecheng0214@hotmail.com>
2022-04-05 04:50:48 +0200

    BlockState Documentation - done

--- workingProject/src/breakout/BlockState.java
+++ workingProject/src/breakout/BlockState.java
@@ -1,35 +1,147 @@
 package breakout;
 
+
+//@invar | getSize().getX() == getBlockBR().getX() - getBlockTL().getX() || getSize().getX() == 0
+//@invar | getSize().getY() == getBlockBR().getY() - getBlockTL().getY() || getSize().getY() == 0
+/**
+ * 
+ * Each instance of this class represents a block defined by a top-left point and a bottom-right point.The width and the height of the block is the block's size.
+ * 
+ * @immutable
+ * 
+ * Abstract state invariants
+ * 
+ * @invar | getSize() != null
+ * @invar | getBlockTL() != null
+ * @invar | getBlockBR() != null
+ * @invar | getSize().getX() >= 0 && getSize().getY() >= 0
+ * @invar | ((getBlockBR().getX()<50000 && getBlockBR().getY()<30000)&&(getBlockTL().getX()<50000 && getBlockTL().getY()<30000)&& (getBlockTL().getY()>0 && getBlockTL().getX()>0)&& (getBlockBR().getY()>0 && getBlockBR().getX()>0))||(getBlockTL().equals(new Point(-250,-250))&&getBlockBR().equals(new Point(-250,-250)))
+ * 
+ */
 public class BlockState {
 	// TODO: implement
 	//DONE-check it
-	Point blockTL;
-	Point blockBR;
-	Vector size;
-	BlockState (Point blockTL, Point blockBR,Vector size){
+	// * @invar | size_copy.getX() == blockBR_copy.getX() - blockTL_copy.getX() ||size_copy.getX() == 0
+     //* @invar | size_copy.getY() == blockBR_copy.getY() - blockTL_copy.getY() ||size_copy.getY() == 0
+	 //* @invar | size.getX() == blockBR.getX() - blockTL.getX() || size.getX() == 0
+	 //* @invar | size.getY() == blockBR.getY() - blockTL.getY() || size.getY() == 0
+	/**
+	 * Representation state invariants
+	 * 
+	 * @invar | size != null
+	 * @invar | size_copy != null
+	 * @invar | blockTL != null
+	 * @invar | blockTL_copy != null
+	 * @invar | blockBR != null
+	 * @invar | blockBR_copy != null
+	 * @invar | size.getX() >= 0 && size.getY() >= 0
+	 * @invar | size_copy.getX() >= 0 && size_copy.getY() >= 0
+	 * @invar | ((blockBR.getX()<50000 && blockBR.getY()<30000)&&(blockTL.getX()<50000 && blockTL.getY()<30000)&& (blockTL.getY()>0 && blockTL.getX()>0)&& (blockBR.getY()>0 && blockBR.getX()>0))||(blockTL.equals(new Point(-250,-250))&&blockBR.equals(new Point(-250,-250)))
+	 * @invar | ((blockBR_copy.getX()<50000 && blockBR_copy.getY()<30000)&&(blockTL_copy.getX()<50000 && blockTL_copy.getY()<30000)&& (blockTL_copy.getY()>0 && blockTL_copy.getX()>0)&& (blockBR_copy.getY()>0 && blockBR_copy.getX()>0))||(blockTL_copy.equals(new Point(-250,-250))&&blockBR_copy.equals(new Point(-250,-250)))
+	 * 
+	 * @representationObject
+	 */
+	private final Point blockTL;
+	private final Point blockBR;
+	private final Vector size;
+	private Point blockTL_copy;
+	private Point blockBR_copy;
+	private Vector size_copy;
+	
+	private BlockState (Point blockTL, Point blockBR,Vector size){
 		this.blockBR=blockBR;
 		this.blockTL=blockTL;
 		this.size=size;
+
+		//Is this passing value (content) or passing reference (address)?
+		//shall we do it in this way?
+		this.blockBR_copy=new Point(this.blockBR.getX(),this.blockBR.getY());
+		this.blockTL_copy=this.blockTL;
+		this.size_copy=this.size;
+		
 	};
+	
+	
+	// * @pre | size.getX() == blockBR.getX() - blockTL.getX() || size.getX() == 0
+	// * @pre | size.getY() == blockBR.getY() - blockTL.getY() || size.getY() == 0
+
+	//Factory method
+	/**
+	 * Returns a block object defined by a top-left point and a bottom-right point.The width and the height of the block is the block's size.
+	 * 
+	 * @pre | blockTL != null
+	 * @pre | blockBR != null
+	 * @pre | size != null
+	 * @pre | size.getX() >= 0 && size.getY() >= 0
+	 * @pre |((blockBR.getX()<50000 && blockBR.getY()<30000)&&(blockTL.getX()<50000 && blockTL.getY()<30000)&& (blockTL.getY()>0 && blockTL.getX()>0)&& (blockBR.getY()>0 && blockBR.getX()>0))||(blockTL.equals(new Point(-250,-250))&&blockBR.equals(new Point(-250,-250)))
+	 * @post | result != null
+	 * @post | result.getBlockTL().equals(blockTL) && result.getBlockBR().equals(blockBR) && result.getSize().equals(size)
+	 * 
+	 */
+	public static BlockState valueOf(Point blockTL, Point blockBR,Vector size) {
+		Point blockTL_copy=new Point(blockTL.getX(),blockTL.getY());
+		Point blockBR_copy=new Point(blockBR.getX(),blockBR.getY());
+		Vector size_copy=new Vector(size.getX(),size.getY());
+		return new BlockState(blockTL_copy, blockBR_copy, size_copy);
+	}
+	
+	
+	
+	/**
+	 * 
+	 * @inspects | this
+	 */
 	public Point  getPosition() {
-		int blockcenterx= (blockBR.getX()-blockTL.getX())/2;
-		int blockcentery= (blockTL.getY()-blockBR.getY())/2;
+		int blockcenterx= (blockBR_copy.getX()+blockTL_copy.getX())/2;
+		int blockcentery= (blockTL_copy.getY()+blockBR_copy.getY())/2;
 		Point center=new Point (blockcenterx,blockcentery);
 		return center;
 	};
+	
+	/**
+	 * 
+	 * @inspects | this
+	 */
 	public Vector getSize() {
-		return size;
+		 Vector size_copy=new Vector(this.size_copy.getX(),this.size_copy.getY());
+		 return size_copy;
 	}
+	/**
+	 * 
+	 * @inspects | this
+	 */
 	public Point getBlockTL() {
-		return blockTL;
+		Point blockTL_copy=new Point(this.blockTL_copy.getX(),this.blockTL_copy.getY());
+		return blockTL_copy;
 	}
+	/**
+	 * 
+	 * @inspects | this
+	 */
 	public Point getBlockBR() {
-		return blockBR;
+		Point blockBR_copy=new Point(this.blockBR_copy.getX(),this.blockBR_copy.getY());
+		return blockBR_copy;
 	}
+	
+	/**
+	 * @pre | TL != null
+	 * @pre | (TL.getY()>0 && TL.getX()>0 && TL.getY()<30000 && TL.getX() <50000) || TL.equals(new Point(-250,-250))
+	 * @mutates mutates the copy of the top-left point of the block | getBlockTL()
+	 * @post | TL.equals(getBlockTL())
+	 */
 	public void setBlockTL(Point TL) {
-		blockTL=TL;
+		Point TL_copy=new Point(TL.getX(),TL.getY());
+		this.blockTL_copy=TL_copy;
 	}
+	
+	/**
+	 * @pre | BR != null
+	 * @pre | (BR.getY()>0 && BR.getX()>0 && BR.getY()<30000 && BR.getX() <50000) || BR.equals(new Point(-250,-250))
+	 * @mutates mutates the copy of the bottom-right point of the block | getBlockBR()
+	 * @post | BR.equals(getBlockBR())
+	 */
 	public void setBlockBR(Point BR) {
-		blockBR=BR;
+		Point BR_copy=new Point(BR.getX(),BR.getY());
+		this.blockBR_copy=BR_copy;
 	}
 }
--- workingProject/src/breakout/BlockStateTest.java
+++ workingProject/src/breakout/BlockStateTest.java
@@ -8,7 +8,41 @@ class BlockStateTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		int HEIGHT = 30000;
+		int WIDTH = 50000;
+		int BLOCK_LINES = 9;
+		int BLOCK_COLUMNS = 10;
+
+		Vector size = new Vector(WIDTH/BLOCK_COLUMNS-70,HEIGHT/BLOCK_LINES-70);
+		Point blockTL = new Point (20,20);
+		Point blockBR = blockTL.plus(size);
+		
+		BlockState myBlock1 =BlockState.valueOf(blockTL,blockBR,size);
+		//BlockState myBlock2 =BlockState.valueOf(new Point(-20,-30),blockBR,size);
+		//BlockState myBlock3 =BlockState.valueOf(blockTL,new Point (-70,-100),size);
+		//BlockState myBlock4 =BlockState.valueOf(null,blockBR,size);
+		//BlockState myBlock5 =BlockState.valueOf(blockTL,null,size);
+		//BlockState myBlock6 =BlockState.valueOf(blockTL,blockBR,null);
+		//BlockState myBlock7 =BlockState.valueOf(new Point (60000,60000),blockBR,size);
+		//BlockState myBlock8 =BlockState.valueOf(blockTL,new Point (60000,60000),size);
+		//BlockState myBlock9 =BlockState.valueOf(blockTL,blockBR,new Vector (-30,-30));
+		
+		//myBlock1.setBlockTL(null);
+		//myBlock1.setBlockBR(null);
+		//myBlock1.setBlockBR(new Point (-250,-250));
+		//myBlock1.setBlockTL(new Point (-250,-250));
+		//myBlock1.setBlockBR(new Point (10000,-100));
+		//myBlock1.setBlockBR(new Point (100000,200));
+		//myBlock1.setBlockTL(new Point (10000,-100));
+		//myBlock1.setBlockTL(new Point (100000,200));
+		
+		
+		BlockState myBlock10 = BlockState.valueOf(new Point(100,100),new Point(2100,2100),new Vector (2000,2000));
+		assertEquals(new Point(1100,1100),myBlock10.getPosition());
+		assertEquals(new Vector(2000,2000),myBlock10.getSize());
+		assertEquals(new Point(1100,1100),myBlock10.getPosition());
+		assertEquals(new Point(100,100),myBlock10.getBlockTL());
+		assertEquals(new Point(2100,2100),myBlock10.getBlockBR());
 	}
 
 }
