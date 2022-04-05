f7f1a593e34bb612d1eb240dd1060bdaa47e08a2
Yuke Cheng <yukecheng0214@hotmail.com>
2022-04-05 07:47:34 +0200

    PaddleState documentation - done

--- workingProject/src/breakout/PaddleState.java
+++ workingProject/src/breakout/PaddleState.java
@@ -1,22 +1,84 @@
 package breakout;
 
+/**
+ * Each instance of this class represents a paddle defined by its central position and size.
+ * 
+ * @immutable
+ * 
+ * Abstract state invariants
+ * 
+ * @invar | getPosition() !=null
+ * @invar | getSize() !=null
+ * @invar | getPosition().getX()>=0 && getPosition().getY()>=0 && getPosition().getX()<=30000 && getPosition().getY()<=50000
+	
+ * 
+ */
 public class PaddleState {
 	// TODO: implement
 	//DONE-check it
-	private Point position;
-	private Vector size;
-	public PaddleState (Point position, Vector size){
+	/**
+	 * Representation state invariants
+	 * 
+	 * @invar | position != null
+	 * @invar | size != null
+	 * @invar | position.getX()>=0 && position.getY()>=0 && position.getX()<=30000 && position.getY()<=50000
+	 * 
+	 * @representationObject
+	 */
+	private final Point position;
+	private final Vector size;
+	private Point position_copy;
+	
+	private PaddleState (Point position, Vector size){
 		this.position=position;
 		this.size=size;
+		
+		this.position_copy= new Point(this.position.getX(),this.position.getY());
+		//this.position_copy=this.position;
 	};
-	public Point  getPosition() {
-		return position;
-	};
-	public Vector getSize() {
-		return size;
+	
+	//Factory method
+	
+	/**
+	 * Returns a paddle object defined by its central position and size.
+	 * 
+	 * @pre | position != null
+	 * @pre | size != null
+	 * @pre | size.getX() > 0 && size.getY() > 0
+	 * @pre | position.getX()>=0 && position.getY()>=0 && position.getX()<=30000 && position.getY()<=50000
+	 * @post | result != null
+	 * @post | result.getPosition().equals(position) && result.getSize().equals(size)
+	 * 
+	 */
+	public static PaddleState valueOf (Point position, Vector size) {
+		Point position_copy=new Point(position.getX(),position.getY());
+		Vector size_copy=new Vector (size.getX(),size.getY());
+		return new PaddleState (position_copy,size_copy);
 	}
+	/**
+	 * @inspects | this
+	 * 
+	 */
+	public Point  getPosition() {
+		return new Point(this.position_copy.getX(),this.position_copy.getY());
+	};
+	/**
+	 * @inspects | this
+	 * 
+	 */
+	public Vector getSize() {
+		return new Vector(this.size.getX(),this.size.getY());
+	}
+	
+	/**
+	 * @pre | position != null
+     * @pre | position.getX()>=0 && position.getY()>=0 && position.getX()<=30000 && position.getY()<=50000
+	 * @mutates mutates the copy of the central position of the paddle | getPosition()
+	 * @post | position.equals(getPosition())
+	 *
+	 */
 	public void setPosition(Point position) {
-		this.position=position;
+		this.position_copy=new Point(position.getX(),position.getY());
 
 	};
 }
--- workingProject/src/breakout/PaddleStateTest.java
+++ workingProject/src/breakout/PaddleStateTest.java
@@ -8,7 +8,32 @@ class PaddleStateTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		int HEIGHT = 30000;
+		int WIDTH = 50000;
+		int BLOCK_LINES = 9;
+		int BLOCK_COLUMNS = 10;
+		
+		Vector size = new Vector(WIDTH/BLOCK_COLUMNS/2,HEIGHT/BLOCK_LINES/2);
+		Point center = new Point(0,0).plus(size);
+		
+		PaddleState myPaddle1= PaddleState.valueOf(center,size);
+		//PaddleState myPaddle2= PaddleState.valueOf(null,size);
+		//PaddleState myPaddle3= PaddleState.valueOf(center,null);
+		//PaddleState myPaddle4= PaddleState.valueOf(center,new Vector(-10,-10));
+		//PaddleState myPaddle5= PaddleState.valueOf(new Point(-100,-100),size);
+		//PaddleState myPaddle6= PaddleState.valueOf(new Point(100000,100000),size);
+		
+		PaddleState myPaddle10= PaddleState.valueOf(new Point(5000,5000),new Vector(1000,1000));
+		assertEquals(new Point(5000,5000),myPaddle10.getPosition());
+		assertEquals(new Vector(1000,1000), myPaddle10.getSize());
+		
+		//myPaddle10.setPosition(null);
+		//myPaddle10.setPosition(new Point(-100,-100));
+		//myPaddle10.setPosition(new Point(100000,100000));
+		myPaddle1.setPosition(new Point(2000,2000));
+		assertEquals(new Point(2000,2000),myPaddle1.getPosition());
+		
+		
 	}
 
 }
