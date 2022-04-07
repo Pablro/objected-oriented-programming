8bd1735d61092d2875a81feb15cd10a4e5076219
Yuke Cheng <yukecheng0214@hotmail.com>
2022-04-07 23:21:50 +0200

    Yuke checked the Ball/Paddle/Block tests

--- workingProject/src/breakout/BallStateTest.java
+++ workingProject/src/breakout/BallStateTest.java
@@ -16,20 +16,21 @@ class BallStateTest {
 		//BallState ballstate3=BallState.valueOfA(new Point(30,70),700,null);
 		//BallState ballstate4=BallState.valueOfA(null,700,new Vector(5,7));
 		//BallState ballstate5=BallState.valueOfA(new Point(70,70),300,new Vector(5,7));
+		//BallState ballstate6=BallState.valueOfA(new Point(-250,-250),700,new Vector(5,7));
 		//Correct Argument
-		BallState ballstate6=BallState.valueOfA(new Point(70,70),700,new Vector(5,7));
+		BallState ballstate7=BallState.valueOfA(new Point(70,70),700,new Vector(5,7));
 	}
 	@Test
 	void testInvalidArgumentsValueOfB() {
 		//Incorrect Arguments
-		//BallState ballstate1=BallState.valueOfB(new Point(0,0),new Vector(700/2,700/2),new Vector(5,7));
-		//BallState ballstate2=BallState.valueOfB(new Point(30,70),new Vector(700/2,700/2),new Vector(12,7));
+		BallState ballstate1=BallState.valueOfB(new Point(0,0),new Vector(700/2,700/2),new Vector(5,7));
 		//BallState ballstate3=BallState.valueOfB(new Point(30,70),new Vector(700/2,700/2),null);
 		//BallState ballstate4=BallState.valueOfB(null,new Vector(700/2,700/2),new Vector(5,7));
 		//BallState ballstate5=BallState.valueOfB(new Point(70,70),new Vector(300/2,300/2),new Vector(5,7));
+		//BallState ballstate6=BallState.valueOfB(new Point(-250,-250),new Vector(700/2,700/2),new Vector(5,7));
 		
 		//Correct Argument
-		BallState ballstate6=BallState.valueOfB(new Point(70,70),new Vector(700/2,700/2),new Vector(5,7));
+		BallState ballstate7=BallState.valueOfB(new Point(70,70),new Vector(700/2,700/2),new Vector(5,7));
 		
 	}
 	@Test
@@ -40,14 +41,13 @@ class BallStateTest {
 	 * */
 	 
 	void testUpdatingValues() {
-		/*BallState ballState= BallState.valueOfA(new Point(30,70),700,new Vector(5,7));
+		/*
+		BallState ballState= BallState.valueOfA(new Point(30,70),700,new Vector(5,7));
 		ballState=ballState.getNewVelocity(new Vector(3,7));
 		assertEquals(new Vector(3,7),ballState.getVelocity());
-		ballState=ballState.getNewSize(new Vector(0,0));
-		assertEquals(new Vector(0,0),ballState.getSize());
 		ballState=ballState.getNewPosition(new Point(80,100));
 		assertEquals(new Point(80,100),ballState.getCenter());
-		ballState=ballState.getNewPosition(new Point(-250,-250));
-		assertEquals(new Point(-250,-250),ballState.getCenter());*/
+		*/
+
 	}
 }
--- workingProject/src/breakout/BlockStateTest.java
+++ workingProject/src/breakout/BlockStateTest.java
@@ -27,10 +27,11 @@ class BlockStateTest {
 		//BlockState myBlock7 =BlockState.valueOf(new Point (60000,60000),blockBR,size);
 		//BlockState myBlock8 =BlockState.valueOf(blockTL,new Point (60000,60000),size);
 		//BlockState myBlock9 =BlockState.valueOf(blockTL,blockBR,new Vector (-30,-30));
+		//BlockState myBlock10 =BlockState.valueOf(new Point (-250,-250),new Point (-250,-250),size);
 		}
 	@Test
 	void testUpdatingValues() {
-		/*
+		
 		int HEIGHT = 30000;
 		int WIDTH = 50000;
 		int BLOCK_LINES = 9;
@@ -42,18 +43,7 @@ class BlockStateTest {
 		assertEquals(size,myBlock10.getSize());
 		assertEquals(new Point(100,100),myBlock10.getBlockTL());
 		assertEquals(new Point(2100,2100),myBlock10.getBlockBR());
-		myBlock10=myBlock10.setBlockTLBR(new Point(-250,-250), new Point(-250,-250));
-		assertEquals(new Point(-250,-250),myBlock10.getBlockBR());
-		assertEquals(new Point(-250,-250),myBlock10.getBlockTL());
-		myBlock10=myBlock10.setBlockTLBR(new Point(200,200), new Point(2200,2200));
-		assertEquals(new Point(200,200),myBlock10.getBlockTL());
-		assertEquals(new Point(2200,2200),myBlock10.getBlockBR());
-		//Incorrect Argument
-		//myBlock10=myBlock10.setBlockTLBR(new Point(100000,200), new Point(300,400));
-		//myBlock10=myBlock10.setBlockTLBR(new Point(300,400), new Point(-5,400));
-		//myBlock10=myBlock10.setBlockTLBR(null, null);
-		 * */
-		 
+		
 	}
 
 }
